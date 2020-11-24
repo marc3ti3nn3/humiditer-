4085e34b66e100937f3d20deb779d15c7714a2eb
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-11-24 14:53:19 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ let reading = 0
 led.setBrightness(64)
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 1023)
-    reading = pins.digitalReadPin(DigitalPin.P0)
+    reading = pins.analogReadPin(AnalogPin.P0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     led.plotBarGraph(
     reading,
     1023
     )
-    if (input.buttonIsPressed(Button.A)) {
+    if (input.buttonIsPressed(Button.B)) {
         basic.showNumber(reading)
     }
-    basic.pause(1000)
+    basic.pause(5000)
 })
