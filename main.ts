b0801c13ebe09e440cd7e9ec6e960ea20a777615
marc3ti3nn3 <73190943+marc3ti3nn3@users.noreply.github.com>
2020-11-24 14:27:52 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+let reading = 0
+led.setBrightness(64)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 1023)
+    reading = pins.digitalReadPin(DigitalPin.P0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
+    }
+    basic.pause(1000)
+})
